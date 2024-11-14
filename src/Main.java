53f015bd9b3d635e6c0bc546d10b7a78d8c32ce4
kirill <kirillstefanov423@gmail.com>
2024-11-14 13:50:57 +0600

    HomeWork 10

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println();
+        System.out.println("Задание 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println();
+        System.out.println("Задание 3");
+
+        String fullName3 = "Иванов Семён Семёнович";
+        System.out.println(fullName3.replace("ё", "е"));
+    }
+}
